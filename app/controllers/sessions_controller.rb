3cdcc0043aa77bdce7e367318a3e9b17df52b8d1
Luca <lucakanyo@protonmail.com>
2020-07-13 12:16:31 -0400

    setting up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,41 @@
+require 'pry'
 class SessionsController < ApplicationController
     get '/signup' do 
-        "signup"
+        erb :'sessions/signup'
     end 
+
+    post '/signup' do 
+        @user = User.new(username: params[:user][:username], password: params[:user][:password])
+        if @user.save
+            erb :'sessions/login'
+        else 
+            erb :'sessions/error' 
+        end 
+    end 
+
+    get '/login' do 
+        erb :'sessions/login'
+    end 
+
+    get '/error' do 
+        erb :'sessions/error'
+    end 
+
+    post '/login' do 
+        @user = User.find_by(username: params[:user][:username])
+
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            erb :'sessions/success'
+        else 
+            redirect to '/error'
+        end 
+    end 
+
+    get '/logout' do 
+        session.clear
+        redirect to '/'
+    end 
+
 
 end 
